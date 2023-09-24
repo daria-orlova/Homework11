12ed18e9817d74c2d4567f48d4d08b6b12368c83
dariaorlova <orlova77m@gmail.com>
2023-09-24 16:32:37 +0300

    Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        System.out.println();
+
+        System.out.println("Задача 2");
+        System.out.println();
+
+        System.out.println("Задача 3");
+    }
+}
